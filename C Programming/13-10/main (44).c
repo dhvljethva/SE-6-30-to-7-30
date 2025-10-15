b23854e82d935a9bf774acbcd004da2ce25de093
dhvljethva <jethvadhaval38390@gmail.com>
2025-10-15 11:21:45 +0530

    Add files via upload

---
+++ C Programming/13-10/main (44).c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+void main()
+{  
+    
+    int i;
+    
+    for(i=1;i<10;i++)
+    
+    {
+        if(i==5)
+        {
+        break;
+    }
+    printf("\n %d",i);//1//2//3//4
+    
+        
+    }
+    getch();
+}
