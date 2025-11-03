ef09e553fe624b954475e29bbb24dd23fb72a08b
dhvljethva <jethvadhaval38390@gmail.com>
2025-11-03 13:30:42 +0530

    Add files via upload

---
+++ C Programming/31-10/main (69).c
@@ -0,0 +1,25 @@
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    
+    int age[5];
+    
+    age[0]=10;
+    age[1]=11;
+    age[2]=12;
+    age[3]=13;
+    age[4]=14;  
+    
+    for(int i=0;i<5;i++)
+    
+    {
+        printf("\n %d",age[i]);
+    }
+    
+
+     getch();
+}
+
---
+++ C Programming/31-10/main (70).c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    
+    int age[5]={10,20,30,40,50};
+    
+    for(int i=0;i<5;i++)
+    
+    {
+        printf("\n %d",age[i]);
+    }
+    
+
+     getch();
+}
+
