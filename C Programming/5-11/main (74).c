d07807dc26ad588d4b04a7fa44e327d95abab11d
dhvljethva <jethvadhaval38390@gmail.com>
2025-11-06 13:28:06 +0530

    Add files via upload

---
+++ C Programming/5-11/main (74).c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+   int number=5,i;
+  
+  
+   int marks[number];
+   
+   for(i=0;i<number;i++)
+   {
+       printf("enter your marks for subject");
+       scanf("%d",&marks[i]);
+       
+    }
+   
+    return 0;
+}
